c034596a68268888da42cde60303bd3b2547acec
Shiva4113 <shivagolugula123@gmail.com>
2024-03-28 19:15:01 +0530

    28-03-2024

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,jsonify
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')#https://localhost:5000
 def home():
     # return jsonify({})
     pass
@@ -12,18 +12,22 @@ def process_asr(audioContent,srcLang,serviceID):
 def process_nmt(textContent,srcLang,tgtLang,serviceID):
     pass
 
-@app.route('/process',methods = ["POST","GET"])
+@app.route('/process',methods = ["POST","GET"])#https://localhost:5000/process
 def process_request():
     data = {}
     if request.method == "POST":
         #here i will get all the data, after that i will call process asr and process nmt ??
         data = request.json
-        audioContent = data['inputData']['audio'][0]['audioContent']
-        srcLang = data['pipelineTasks'][0]['config']['language']['sourceLanguage']
-        tgtLang = data['pipelineTasks'][1]['config']['language']['targetLanguage']
-        asrServiceID = data['pipelineTasks'][0]['config']['serviceId']
-        nmtServiceID = data['pipelineTasks'][0]['config']['serviceId']
+        # audioContent = data['inputData']['audio'][0]['audioContent']
+        # srcLang = data['pipelineTasks'][0]['config']['language']['sourceLanguage']
+        # tgtLang = data['pipelineTasks'][1]['config']['language']['targetLanguage']
+        # asrServiceID = data['pipelineTasks'][0]['config']['serviceId']
+        # nmtServiceID = data['pipelineTasks'][0]['config']['serviceId']
+        # pass
+        name = data["name"]
+
     return data
 
+    #post request will be made to api -> response from that will be then processes by me to my LLM -> classification of received comamnd, prompt will be a mapping prompt -> this will in turn make a call to handle the database in some manner
 if __name__ == "__main__":
     app.run(debug=True)
