f188f67f6969af397704c9c98578fe0272cb6acc
Shiva Golugula <121237535+Shiva4113@users.noreply.github.com>
2024-03-29 13:23:19 +0530

    Delete endpoints.py

--- endpoints.py
+++
@@ -1,3 +0,0 @@
-GET_SERVICE_ENDPOINT = "https://meity-auth.ulcacontrib.org/ulca/apis/v0/model/getModelsPipeline"
-ASR_NMT_ENDPOINT = "https://dhruva-api.bhashini.gov.in/services/inference/pipeline"
-NMT_TTS_ENDPOINT = ""
