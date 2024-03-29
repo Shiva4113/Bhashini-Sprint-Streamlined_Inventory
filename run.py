84eb2294ec4aad223c18ab7519a9c710c11027e0
sidkans <sid_kans@hotmail.com>
2024-03-29 13:22:22 +0530

    nmttts fixed

--- run.py
+++ run.py
@@ -55,13 +55,14 @@ model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                               safety_settings=safety_settings)
 
 #MONGODB SETUP
+'''
 try:
     client = MongoClient(mongoURI)
     dbStores = client.stores
     storeDetails = dbStores.details
 except ConnectionFailure as e:
     print({"Connnection Error": e})
-
+'''
 #FLASK 
 app = Flask(__name__)
 
@@ -80,55 +81,106 @@ def process_instr(instruction):
     pass
 
 def process_asr_nmt(audioContent,srcLang,tgtLang,asrServiceId,nmtServiceId):
-    pipelineTasks = [
-        {
-            "taskType": "asr",
-            "config": {
-                "language": {
-                    "sourceLanguage": srcLang
-                },
-                "serviceId": asrServiceId,
-                "audioFormat": "flac",
-                "samplingRate": 16000
-            }
-        },
-        {
-            "taskType": "translation",
-            "config": {
-                "language": {
-                    "sourceLanguage": srcLang,
-                    "targetLanguage": tgtLang
-                },
-                "serviceId": nmtServiceId
-            }
-        }
-    ]
-    
-    inputData = {
-        "audio": [
+    try:
+        pipelineTasks = [
             {
-                "audioContent": audioContent
+                "taskType": "asr",
+                "config": {
+                    "language": {
+                        "sourceLanguage": srcLang
+                    },
+                    "serviceId": asrServiceId,
+                    "audioFormat": "flac",
+                    "samplingRate": 16000
+                }
+            },
+            {
+                "taskType": "translation",
+                "config": {
+                    "language": {
+                        "sourceLanguage": srcLang,
+                        "targetLanguage": tgtLang
+                    },
+                    "serviceId": nmtServiceId
+                }
             }
         ]
-    }
+        
+        inputData = {
+            "audio": [
+                {
+                    "audioContent": audioContent
+                }
+            ]
+        }
 
-    reqPayload = {
-        "pipelineTasks": pipelineTasks,
-        "inputData": inputData
-    }
+        reqPayload = {
+            "pipelineTasks": pipelineTasks,
+            "inputData": inputData
+        }
 
-    headers = {
-        computeAuthKey:computeAuthValue
-    }
+        headers = {
+            computeAuthKey:computeAuthValue
+        }
 
-    responseAsrNmt = requests.post(ASR_NMT_TTS_ENDPOINT,json=reqPayload,headers=headers)
+        responseAsrNmt = requests.post(ASR_NMT_TTS_ENDPOINT,json=reqPayload,headers=headers)
+
     
-    instruction = responseAsrNmt.json()["pipelineResponse"][1]["output"][0]["target"]
-    return process_instr(instruction=instruction)
+    except Exception as e:
+        return {"error":str(e)},500
+    
+    finally:
+        return responseAsrNmt.json()
+        
 
+def process_nmt_tts(sampleInput,srcLang,tgtLang,ttsServiceId,nmtServiceId):
+    try:
+        pipelineTasks = [
+            {
+                "taskType": "translation",
+                "config": {
+                    "language": {
+                        "sourceLanguage": srcLang,
+                        "targetLanguage": tgtLang
+                    },
+                    "serviceId": nmtServiceId
+                }
+            },
+            {
+                "taskType": "tts",
+                "config": {
+                    "language": {
+                        "sourceLanguage": tgtLang
+                    },
+                    "serviceId": ttsServiceId,
+                    "gender": "female",
+                    "samplingRate": 8000
+                }
+            }
+        ]
+        
+        inputData = {
+            "input": [
+                {
+                    "source": sampleInput
+                }
+            ]
+        }
+        
+        reqPayload = {
+            "pipelineTasks": pipelineTasks,
+            "inputData": inputData
+        }
 
-def process_nmt_tts():
-    pass
+        headers = {
+            computeAuthKey:computeAuthValue
+        }
+
+        responseNmtTts = requests.post(ASR_NMT_TTS_ENDPOINT,json=reqPayload,headers=headers)
+    except Exception as e:
+        return {"error":str(e)},500
+    finally:
+        return responseNmtTts.json()
 
 def process_ocr():
     pass
@@ -197,10 +249,16 @@ def process_request():
         
         asrServiceId = responseServices.json()["pipelineResponseConfig"][0]["config"][0]["serviceId"]#this correctly gives the asr service ID to us
         nmtServiceId = responseServices.json()["pipelineResponseConfig"][1]["config"][0]["serviceId"]#this correctly gives the nmt service ID to us
+        ttsServiceId = responseServices.json()["pipelineResponseConfig"][2]["config"][0]["serviceId"]#this correctly gives the tts service ID to us
     
         responseAsrNmt=process_asr_nmt(audioContent=audioContent,srcLang=srcLang,tgtLang=tgtLang,asrServiceId=asrServiceId,nmtServiceId=nmtServiceId)
+        responseNmtTts=process_nmt_tts(sampleInput = "hello",srcLang=srcLang,tgtLang=tgtLang,ttsServiceId=ttsServiceId,nmtServiceId=nmtServiceId)
 
-        return responseAsrNmt
+        tempresp = {
+            "responseAsrNmt" : responseAsrNmt,
+            "responseNmtTts" : responseNmtTts
+        }
+        return tempresp
         
 
     #post request will be made to api -> response from that will be then processes by me to my LLM -> classification of received comamnd, prompt will be a mapping prompt -> this will in turn make a call to handle the database in some manner
